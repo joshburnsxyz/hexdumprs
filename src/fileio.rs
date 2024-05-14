2459bfb610d4b56267dff968a46fa2935f54f3f9
Josh Burns <joshyburnss@gmail.com>
2024-05-15 08:37:07 +1000

    implement error handling on read_file

--- src/fileio.rs
+++ src/fileio.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
-use std::io;
+use std::io::{self, Error, ErrorKind};
 
 pub fn read_file(file_path: &str) -> io::Result<File> {
-    File::open(file_path)
+    match File::open(file_path) {
+	Ok(file) => Ok(file),
+	Err(err) => {
+	    let errmsg = format!("Failed to open file '{}': {}", file_path, err);
+	    return Err(Error::new(ErrorKind::NotFound, errmsg));
+	}
+    }
 }
